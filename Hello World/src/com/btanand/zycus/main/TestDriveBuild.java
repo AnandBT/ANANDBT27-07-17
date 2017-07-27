3e7ae0f7ccfee7e332034ef3317ef7581c84b381
AnandBT <btanand40@gmail.com>
2017-07-27 23:49:45 +0530

    Add files via upload
    Committing the Solution to Question 1 of Zycus Assessment.

---
+++ Hello World/src/com/btanand/zycus/main/TestDriveBuild.java
@@ -0,0 +1,24 @@
+package com.btanand.zycus.main;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestDriveBuild extends HttpServlet {
+
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8198549592206783255L;
+
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)  { 		
+			try {	
+				response.getWriter().print("<h1>Hello World</h1>");
+			} catch (Exception e) {
+				System.err.println("Fatal Servlet Error");
+				e.printStackTrace();
+			}			
+		}
+		
+}
